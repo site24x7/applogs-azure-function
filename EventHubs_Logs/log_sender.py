02d36b60eba5bf8cc2b55819529ad527b5d44576
mageshrajan <100208503+mageshrajan@users.noreply.github.com>
2022-12-04 23:31:02 +0530

    Update log_sender.py
    Masking, Hashing, and Derived fields support added

--- EventHubs_Logs/log_sender.py
+++ EventHubs_Logs/log_sender.py
@@ -1,10 +1,11 @@
 
-import sys, os, re, gzip, json, urllib.parse, urllib.request, traceback, datetime, calendar, logging
+import sys, os, re, gzip, json, urllib.parse, urllib.request, traceback, datetime, calendar, logging, hashlib, ast
 import azure.functions as func
 from base64 import b64decode
 
 logtype_config = None
 s247_datetime_format_string = None
+log_size=0
 
 def get_timestamp(datetime_string):
     try:
@@ -17,9 +18,21 @@ def get_timestamp(datetime_string):
 def is_filters_matched(formatted_line):
     if 'filterConfig' in logtype_config:
         for config in logtype_config['filterConfig']:
-            if config in formatted_line and (filter_config[config]['match'] ^ (formatted_line[config] in filter_config[config]['values'])):
+            if re.findall(logtype_config['filterConfig'][config]['values'],formatted_line[config]) :
+                val = True 
+            else:
+                val = False
+            if config in formatted_line and (logtype_config['filterConfig'][config]['match'] ^ (val)):
                 return False
     return True
+
+def log_line_filter(formatted_line):
+    if masking_config:
+        apply_masking(formatted_line)
+    if hashing_config:
+        apply_hashing(formatted_line)
+    if derived_eval:
+        derivedFields(formatted_line)
 
 def get_json_value(obj, key, datatype=None):
     if key in obj or key.lower() in obj:
@@ -43,22 +56,25 @@ def get_json_value(obj, key, datatype=None):
         return get_json_value(child_obj, child_key)
 
 def json_log_parser(lines_read):
-    log_size = 0
+    global log_size
     parsed_lines = []
     for event_obj in lines_read:
         try:
             formatted_line = {}
+            json_log_size = 0
             for path_obj in logtype_config['jsonPath']:
                 value = get_json_value(event_obj, path_obj['key' if 'key' in path_obj else 'name'], path_obj['type'] if 'type' in path_obj else None)
                 if value:
-                    formatted_line[path_obj['name']] = value 
-                    log_size+= len(str(value))
+                    formatted_line[path_obj['name']] = value
+                    json_log_size+= len(str(value))
             if not is_filters_matched(formatted_line):
                 continue
+            log_size += json_log_size
             formatted_line['_zl_timestamp'] = get_timestamp(event_obj[logtype_config['dateField']])
             if 'resourceId' in event_obj:
                 formatted_line['s247agentuid'] = event_obj['resourceId'].split('/')[4]
                 event_obj['resourceId'] = event_obj['resourceId'].lower()
+            log_line_filter(formatted_line)
             parsed_lines.append(formatted_line)
         except Exception as e:
             print('unable to parse event message : ',event_obj)
@@ -79,9 +95,79 @@ def send_logs_to_s247(gzipped_parsed_lines, log_size):
     else:
         logging.info('%s :Problem in uploading to site24x7 status %s, Reason : %s', dict_responseHeaders['x-uploadid'], s247_response.status, s247_response.read())
 
+def apply_masking(formatted_line):
+    global log_size
+    try:
+        for config in masking_config:
+            adjust_length = 0
+            mask_regex = masking_config[config]['regex']
+            if config in formatted_line:
+                field_value = str(formatted_line[config])
+                for matcher in re.finditer(mask_regex, field_value):
+                    if matcher:
+                        for i in range(mask_regex.groups):
+                            matched_value = matcher.group(i + 1)
+                            if matched_value:
+                                start = matcher.start(i + 1)
+                                end = matcher.end(i + 1)
+                                if start >= 0 and end > 0:
+                                    start = start - adjust_length
+                                    end = end - adjust_length
+                                    adjust_length += (end - start) - len(masking_config[config]['string'])
+                                    field_value = field_value[:start] + masking_config[config]['string'] + field_value[end:]
+                formatted_line[config] = field_value
+                log_size -= adjust_length
+    except Exception as e:
+        traceback.print_exc()
+
+
+def apply_hashing(formatted_line):
+    global log_size
+    try:
+        for config in hashing_config:
+            adjust_length = 0
+            mask_regex = hashing_config[config]['regex']
+            field_value = str(formatted_line[config])
+            if config in formatted_line:
+                for matcher in re.finditer(mask_regex, field_value):
+                    if matcher:
+                        for i in range(mask_regex.groups):
+                            matched_value = matcher.group(i + 1)
+                            if matched_value:
+                                start = matcher.start(i + 1)
+                                end = matcher.end(i + 1)
+                                if start >= 0 and end > 0:
+                                    start = start - adjust_length
+                                    end = end - adjust_length
+                                    hash_string = hashlib.sha256(matched_value.encode('utf-8')).hexdigest()
+                                    adjust_length += (end - start) - len(hash_string)
+                                    field_value = field_value[:start] + hash_string + field_value[end:]
+                formatted_line[config] = field_value
+                log_size -= adjust_length
+    except Exception as e:
+        traceback.print_exc()
+
+
+def derivedFields(formatted_line):
+    global log_size
+    try:
+        for items in derived_fields:
+            for each in derived_fields[items]:
+                if items in formatted_line:
+                    match_derived = each.search(formatted_line[items])
+                    if match_derived:
+                        match_derived_field = match_derived.groupdict(default='-')
+                        formatted_line.update(match_derived_field)
+                        for field_name in match_derived_field:
+                            log_size += len(formatted_line[field_name])
+                        break
+    except Exception as e:
+        traceback.print_exc()
+
+
 def main(eventMessages: func.EventHubEvent):
     try:
-        global logtype_config, s247_datetime_format_string
+        global logtype_config, s247_datetime_format_string,masking_config, hashing_config, derived_eval, derived_fields, log_size
         cardinality = 'many'
         if type(eventMessages) != list:
             eventMessages = [eventMessages]
@@ -91,7 +177,7 @@ def main(eventMessages: func.EventHubEvent):
             log_events = payload['records'] if cardinality == 'many' else payload[0]['records']
 
             log_category = ''
-            if 'category' in log_events[0] or 'Category' in log_events[0]: 
+            if 'category' in log_events[0] or 'Category' in log_events[0]:
                 log_category = (log_events[0]['category' if 'category' in log_events[0] else 'Category']).replace('-', '_')
                 print("log_category" + " : "+ log_category)
                 log_category = 'S247_'+log_category
@@ -105,10 +191,37 @@ def main(eventMessages: func.EventHubEvent):
                 s247_datetime_format_string = logtype_config['dateFormat']
             else:
                 return
-    
+            masking_config = logtype_config['maskingConfig'] if 'maskingConfig' in logtype_config else None
+            hashing_config = logtype_config['hashingConfig'] if 'hashingConfig' in logtype_config else None
+            derived_eval = logtype_config['derivedConfig'] if 'derivedConfig' in logtype_config else None
+
+            if derived_eval:
+                try:
+                    derived_fields = {}
+                    for key in derived_eval:
+                        derived_fields[key] = []
+                        for values in derived_eval[key]:
+                            derived_fields[key].append(re.compile(values.replace('\\\\', '\\').replace('?<', '?P<')))
+                except Exception as e:
+                    print("Error in dfields")
+            if masking_config:
+                for key in masking_config:
+                    masking_config[key]["regex"] = re.compile(masking_config[key]["regex"])
+
+            if hashing_config:
+                for key in hashing_config:
+                    hashing_config[key]["regex"] = re.compile(hashing_config[key]["regex"])
+            
+            if "filterConfig" in logtype_config:
+                for field in logtype_config['filterConfig']:
+                    temp = []
+                    for value in logtype_config['filterConfig'][field]['values']:
+                        temp.append(re.compile(value))
+                    logtype_config['filterConfig'][field]['values'] = '|'.join(x.pattern for x in temp)
+                    
             if 'jsonPath' in logtype_config:
                 parsed_lines, log_size = json_log_parser(log_events)
-    
+
             if parsed_lines:
                 gzipped_parsed_lines = gzip.compress(json.dumps(parsed_lines).encode())
                 send_logs_to_s247(gzipped_parsed_lines, log_size)
